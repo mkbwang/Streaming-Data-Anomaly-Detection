bd9d88f54d025136ad2bbeec9ae0aac3d30a2f08
涂凯巍 <kaiweitu@umich.edu>
2019-06-25 23:08:19 +0800

    Remove build dir

--- scheduler/src/main/java/datapipeline/AnomalyDetectionJob.java
+++
@@ -1,4 +0,0 @@
-package datapipeline;
-
-public class AnomalyDetectionJob {
-}
--- scheduler/src/main/java/datapipeline/streamer/MatrixThresholdLamda.java
+++ scheduler/src/main/java/datapipeline/streamer/MatrixThresholdLamda.java
@@ -29,6 +29,12 @@ import java.util.Properties;
 
 public class MatrixThresholdLamda implements Runnable {
 
+    private int _ID;
+
+    public MatrixThresholdLamda(int ID) {
+        _ID = ID;
+    }
+
     public void run() {
         final Properties streamsConfiguration = new Properties();
         final Serde<String> stringSerde = Serdes.String();
@@ -38,13 +44,16 @@ public class MatrixThresholdLamda implements Runnable {
 
         // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
         // against which the application is run.
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "anomaly-detection-lambda-exam");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "anomaly-detection-lambda-example-client");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "anomaly-detection-streamer-application-" + _ID);
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "anomaly-detection-streamer-" + _ID);
+
         // Where to find Kafka broker(s).
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKERS);
+
         // Specify default (de)serializers for record keys and for record values.
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName()); // Set the commit interval to 500ms so that any changes are flushed frequently. The low latency
+
         // would be important for anomaly detection.
         // streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
         // streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
@@ -92,9 +101,9 @@ public class MatrixThresholdLamda implements Runnable {
                 .filter((key, value) -> Double.parseDouble(value) > 7.0)
                 .toStream()
                 .filter((key, value) ->  value != null)
-                .map(((key, value) -> KeyValue.pair(key.toString(), value)));
+                .map(((key, value) -> KeyValue.pair(key.toString(), key.key())));
 
-        anmalousData.to("result10");
+        anmalousData.to("notification_job_" + _ID);
         final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
         // Always (and unconditionally) clean local state prior to starting the processing topology.
         // We opt for this unconditional call here because this will make it easier for you to play around with the example
