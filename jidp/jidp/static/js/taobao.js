d05a83938468afd3120d31b5c3ded033e518a577
wangmk <wangmk@umich.edu>
2019-07-09 19:50:29 +0800

    A little progress on the website.

--- jidp/jidp/static/js/taobao.js
+++ jidp/jidp/static/js/taobao.js
@@ -1,3 +1,20 @@
 $(document).ready( function() {
-    $( "input" ).checkboxradio();
+    let brandspin = $( "#brandspin" ).spinner();
+    let productspin = $( "#productspin" ).spinner();
+    let categoryspin = $( "#categoryspin" ).spinner();
+    let cspin1 = $( "#customer1" ).spinner();
+    let cspin2 = $( "#customer2" ).spinner();
+    let cspin3 = $( "#customer3" ).spinner();
+    $( "#rainthreshold" ).slider({
+        range: "max",
+        min: 6,
+        max: 18,
+        value: 1,
+        slide: function( event, ui ) {
+          $( "#currbar" ).val( ui.value );
+        }
+      });
+    $( "#currbar" ).val( $( "#rainthreshold" ).slider( "value" ) );
+    $('#submit1').click();
+    $('#submit2').click();
 } );
