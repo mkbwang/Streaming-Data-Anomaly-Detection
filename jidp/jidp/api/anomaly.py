66a4e25517a50c2ea7f0df014bb77132c9e170ea
wangmk <wangmk@umich.edu>
2019-07-11 12:15:43 +0800

    Working on the ajax functionality.

--- jidp/jidp/api/anomaly.py
+++ jidp/jidp/api/anomaly.py
@@ -6,25 +6,31 @@ import numpy as np
 import io
 import jidp
 import base64
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+import json
+from flask import jsonify
 
 @jidp.app.route('/api/anomaly/',methods=["GET"])
 def get_anomaly():
     directory = jidp.model.get_rainfall() # get the directory
-    filename = "example.npy"
-    total_directory = os.path.join(directory, filename)
-    mat = np.load(total_directory)
+    a1 = "a1.json"
+    a1_directory = os.path.join(directory, a1)
+    with open(a1_directory, 'r') as f:
+        anomaly1 = json.load(f)
+    a2 = "a2.json"
+    a2_directory = os.path.join(directory, a2)
+    with open(a2_directory, 'r') as f:
+        anomaly2 = json.load(f)
+    output = {"a1": anomaly1["anomaly"], "a2": anomaly2["anomaly"]}
+    return jsonify(output), 200
     # with open(total_directory, "rb") as img_file:
     #    imgstring = base64.b64encode(img_file.read())
-    plt.figure(figsize=(4, 3))
-    fig, ax = plt.subplots()
-    im = ax.imshow(mat)
-    # store it as a base64 stream and send it
-    strIO = io.BytesIO()
-    plt.savefig(strIO, format='png')
-    strIO.seek(0)
-    plt.close('all')
-    plotcode = base64.encodestring(strIO.read())
-    return plotcode.decode('utf-8'), 200, {'Content-Type': 'text/plain'}
+    # plt.figure(figsize=(4, 3))
+    # fig, ax = plt.subplots()
+    # im = ax.imshow(mat)
+    # # store it as a base64 stream and send it
+    # strIO = io.BytesIO()
+    # plt.savefig(strIO, format='png')
+    # strIO.seek(0)
+    # plt.close('all')
+    # plotcode = base64.encodestring(strIO.read())
+    # return plotcode.decode('utf-8'), 200, {'Content-Type': 'text/plain'}
--- jidp/jidp/static/js/rainalert.js
+++ jidp/jidp/static/js/rainalert.js
@@ -1,40 +1,82 @@
 $(document).ready(function(){
     $('#submit0').click();
-    var dom = document.getElementById("anomalyalert");
-    var myChart = echarts.init(dom);
-    option = {
-        xAxis: {
-            scale: false
-        },
-        yAxis: {
-            scale: false,
-            inverse: true,
-        },
-        series: [{
-            type: 'effectScatter',
-            symbolSize: 10,
-            color:"#ca8622",
-            data: [
-                [400, 105.2],
-                [153.4, 142],
-                [120, 400],
-                [220, 50]
-            ]
-        }, 
-        {
-            type: 'effectScatter',
-            symbolSize: 10,
-            data: [
-                [300, 105.2],
-                [153.4, 182]
-            ]
-        },{
-            type: 'scatter',
-            data: [[500, 0], [0, 500], [500, 500], [0, 0]
-            ],
-        }]
-    };
-    if (option && typeof option === "object") {
-        myChart.setOption(option, true);
-    };
+    function askforalert(){
+        let anomalychart = echarts.init(document.getElementById('anomalyalert'));
+        let option = {
+            xAxis: {
+                scale: false
+            },
+            yAxis: {
+                scale: false,
+                inverse: true,
+            },
+            series: [{
+                type: 'effectScatter',
+                symbolSize: 10,
+                color:"#ca8622",
+                data: [
+                ]
+            }, 
+            {
+                type: 'effectScatter',
+                symbolSize: 10,
+                data: [
+                ]
+            },{
+                type: 'scatter',
+                data: [[500, 0], [0, 500], [500, 500], [0, 0]
+                ],
+            }]
+        };
+        $.ajax({
+            type: "get",
+            url: "/api/anomaly/",
+            dataType : "json",
+            success: function(result){
+                option['series'][0]['data'] = result.a1;
+                console.log(option['series'][0]['data']);
+                option['series'][1]['data'] = result.a2;
+                console.log(option['series'][0]['data']);
+                anomalychart.setOption(option);
+            },
+            error: function(errormsg){
+                console.log("fetch anomaly rainfall failed!")
+            }
+        });
+    }
+    askforalert();
+    // var dom = document.getElementById("anomalyalert");
+    // var myChart = echarts.init(dom);
+    // option = {
+    //     xAxis: {
+    //         scale: false
+    //     },
+    //     yAxis: {
+    //         scale: false,
+    //         inverse: true,
+    //     },
+    //     series: [{
+    //         type: 'effectScatter',
+    //         symbolSize: 10,
+    //         color:"#ca8622",
+    //         data: [
+    //             [400, 105.2],
+    //             [153.4, 142],
+    //             [120, 400],
+    //             [220, 50]
+    //         ]
+    //     }, 
+    //     {
+    //         type: 'effectScatter',
+    //         symbolSize: 10,
+    //         data: [
+    //             [300, 105.2],
+    //             [153.4, 182]
+    //         ]
+    //     },{
+    //         type: 'scatter',
+    //         data: [[500, 0], [0, 500], [500, 500], [0, 0]
+    //         ],
+    //     }]
+    // };
 });
